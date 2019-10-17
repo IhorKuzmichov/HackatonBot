f4b7d7582bf5bca2c92ed81aaef2ec32135263db
Ihor Kuzmichov <37931581+IhorKuzmichov@users.noreply.github.com>
2019-10-17 22:26:23 +0300

    Update AppSettings.cs

--- HackatonBot/Models/AppSettings.cs
+++ HackatonBot/Models/AppSettings.cs
@@ -7,7 +7,7 @@ namespace HackatonBot.Models
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://projecthackaton.herokuapp.com:443/{0}";
+        public static string Url { get; set; } = "https://projecthackatonbot.herokuapp.com/{0}";
         public static string Name { get; set; } = "HackatonBot";
         public static string Key { get; set; } = "877554193:AAEE_C5ixIcqbT511sG3t5rjH9p4nSbg-hU";
     }
