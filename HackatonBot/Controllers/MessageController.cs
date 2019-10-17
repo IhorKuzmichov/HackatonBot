8593b6f28ff89cf1e816662f97f0befd0d1ab1eb
Ihor Kuzmichov <37931581+IhorKuzmichov@users.noreply.github.com>
2019-10-17 22:20:45 +0300

    Add files via upload

---
+++ HackatonBot/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackatonBot.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types;
+
+namespace HackatonBot.Controllers
+{
+    [Route("api/message/update")]
+    public class MessageController : Controller
+    {
+        [HttpGet]
+        public async Task<OkResult> Post([FromBody]Update update)
+        {
+            if (update == null) return Ok();
+
+            var commands = Bot.Commands;
+            var message = update.Message;
+            var botClient = await Bot.GetBotClientAsync();
+
+            foreach (var command in commands)
+            {
+                if (command.Contains(message))
+                {
+                    await command.Execute(message, botClient);
+                    break;
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ HackatonBot/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackatonBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://projecthackaton.herokuapp.com:443/{0}";
+        public static string Name { get; set; } = "HackatonBot";
+        public static string Key { get; set; } = "877554193:AAEE_C5ixIcqbT511sG3t5rjH9p4nSbg-hU";
+    }
+}
